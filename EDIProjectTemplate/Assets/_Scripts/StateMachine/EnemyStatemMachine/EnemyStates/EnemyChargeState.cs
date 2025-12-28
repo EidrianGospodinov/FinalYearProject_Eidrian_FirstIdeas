722bc9cbcd6ab2999a5d10e4140f4dcaa63c1945
Edko <e.d.gospodinov.03@gmail.com>
2025-12-28 19:30:33 +0000

    charge state, modify speed and acceleration

--- EDIProjectTemplate/Assets/_Scripts/StateMachine/EnemyStatemMachine/EnemyStates/EnemyChargeState.cs
+++ EDIProjectTemplate/Assets/_Scripts/StateMachine/EnemyStatemMachine/EnemyStates/EnemyChargeState.cs
@@ -6,6 +6,8 @@ namespace _Scripts.StateMachine.EnemyStatemMachine.EnemyStates
     public class EnemyChargeState : EnemyAttackBaseState
     {
         private Vector3 ChargeDestination;
+        private float previousSpeed;
+        float previousAcceleration;
         public override EnemyStateId GetId()
         {
             return EnemyStateId.Charge;
@@ -15,8 +17,11 @@ namespace _Scripts.StateMachine.EnemyStatemMachine.EnemyStates
         {
             base.Enter(agent);
             agent.navMeshAgent.stoppingDistance = 0;
-            ChargeDestination = agent.playerTransform.transform.position;
-            agent.navMeshAgent.SetDestination(ChargeDestination);
+            previousSpeed = agent.navMeshAgent.speed;
+            agent.navMeshAgent.speed = 18;
+            previousAcceleration = agent.navMeshAgent.acceleration;
+            agent.navMeshAgent.acceleration = 10;
+            SetOvershootDestination(agent,6);
         }
 
         public override void Update(AiAgent agent)
@@ -26,6 +31,21 @@ namespace _Scripts.StateMachine.EnemyStatemMachine.EnemyStates
 
         public override void Exit(AiAgent agent)
         {
+            agent.navMeshAgent.speed = previousSpeed;
+            agent.navMeshAgent.acceleration = previousAcceleration;
+        }
+        public void SetOvershootDestination(AiAgent agent, float extraMeters)
+        {
+            ChargeDestination = agent.playerTransform.transform.position;
+            
+            Vector3 currentPos = agent.transform.position;
+    
+            //Get the direction from AI to Player
+            Vector3 direction = (ChargeDestination - currentPos).normalized;
+            
+            Vector3 overshootPos = ChargeDestination + (direction * extraMeters);
+    
+            agent.navMeshAgent.SetDestination(overshootPos);
         }
     }
 }
