a055dd497251f80639a4af8f469ec85528865d13
Edko03 <121055810+EidrianGospodinov@users.noreply.github.com>
2026-01-16 22:51:56 +0000

    sword highlight material emission intensity and change to URP lit shader

--- EDIProjectTemplate/Assets/_Scripts/Units/Player/WeaponManager.cs
+++ EDIProjectTemplate/Assets/_Scripts/Units/Player/WeaponManager.cs
@@ -8,7 +8,9 @@ namespace _Scripts.Units.Player
     {
         [SerializeField] List<GameObject> SwordPresetList;
         private EventBinding<OnSwitchHeroEvent> playerEventBinding;
-
+        GameObject nextWeaponPreset;
+[Range(0,7)]
+[SerializeField] private float intensity = 2;
         private void OnEnable()
         {
             playerEventBinding = EventBus<OnSwitchHeroEvent>.Register(HandleHeroSwitchEvent);
@@ -17,13 +19,23 @@ namespace _Scripts.Units.Player
         private void HandleHeroSwitchEvent(OnSwitchHeroEvent obj)
         {
             DisableAllSwordPresets();
-            var nextWeaponPreset = SwordPresetList.Find(x => x.name == obj.HeroData.weaponPrefab.name);
+            nextWeaponPreset = SwordPresetList.Find(x => x.name == obj.HeroData.weaponPrefab.name);
             nextWeaponPreset?.SetActive(true);
         }
 
         void DisableAllSwordPresets()
         {
             SwordPresetList.ForEach(x => x.gameObject.SetActive(false));
+        }
+
+        private void Update()
+        {
+            if (nextWeaponPreset != null)
+            {
+                var material=nextWeaponPreset.GetComponent<MeshRenderer>().material;
+                //Mathf.Clamp01( powerupPercent & 7) clamp between 0 and 7 the intensity based on power up loaded level
+                material.SetColor("_EmissionColor", Color.white * Mathf.Pow(2, intensity));
+            }
         }
     }
 }
