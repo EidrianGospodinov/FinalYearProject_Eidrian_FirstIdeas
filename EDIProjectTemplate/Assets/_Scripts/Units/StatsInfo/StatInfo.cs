c6acc77bce0cd19e3ac64bdbd4253d6a4ccdec53
Edko03 <121055810+EidrianGospodinov@users.noreply.github.com>
2026-03-30 21:27:57 +0100

    [iimplementation working on making a stats that i can upgrade

---
+++ EDIProjectTemplate/Assets/_Scripts/Units/StatsInfo/StatInfo.cs
@@ -0,0 +1,8 @@
+using System;
+
+[Serializable]
+public class StatInfo
+{
+    public Stat statType;
+    public float statValue;
+}
---
+++ EDIProjectTemplate/Assets/_Scripts/Units/StatsInfo/Stats.cs
@@ -0,0 +1,73 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Stats/Unit Stats")]
+public class Stats : ScriptableObject
+{
+    public List<StatInfo> instanceStatsInfo = new List<StatInfo>();
+    public List<StatInfo> StatsInfo = new List<StatInfo>();
+    private List<StatsUpgrade> appliedUpgrades = new List<StatsUpgrade>();
+    public float GetStat(Stat stat)
+    {
+        foreach (var statInfo in StatsInfo)
+        {
+            if (statInfo.statType == stat)
+            {
+                return GetUpgradedValue(statInfo.statType, statInfo.statValue);
+            }
+        }
+        foreach (var instanceStatInfo in instanceStatsInfo)
+        {
+            if (instanceStatInfo.statType == stat)
+            {
+                return GetUpgradedValue(instanceStatInfo.statType, instanceStatInfo.statValue);
+            }
+        }
+
+        Debug.LogError($"No stat value found for {stat} on {this.name}");
+        return 0;
+    }
+
+    public void UnlockUpgrade(StatsUpgrade upgrade)
+    {
+        if (!appliedUpgrades.Contains(upgrade))
+        {
+            appliedUpgrades.Add(upgrade);
+        }
+    }
+
+    private float GetUpgradedValue(Stat stat, float baseValue)
+    {
+        foreach (var upgrade in appliedUpgrades)
+        {
+            foreach (var upgradeToApply in upgrade.UpgradeToApply)
+            {
+                if (upgradeToApply.statType != stat)
+                {
+                    continue;
+                }
+
+                if (upgrade.isPercentUpgrade)
+                {
+                    baseValue *= (upgradeToApply.statValue / 100f) + 1f;
+                }
+                else
+                {
+                    baseValue += upgradeToApply.statValue;
+                }
+            }
+        }
+
+        return baseValue;
+    }
+    
+    
+}
+
+public enum Stat
+{
+    health, 
+    speed,
+    switchHeroCooldown,
+    dash
+}
