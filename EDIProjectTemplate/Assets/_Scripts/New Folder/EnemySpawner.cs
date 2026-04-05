382e31b28d23a1857f984220d11ad869b682cc1e
Edko03 <121055810+EidrianGospodinov@users.noreply.github.com>
2026-04-05 12:35:34 +0100

    enemy spawner is modified to use the interactable interface instead of onCollision

--- EDIProjectTemplate/Assets/_Scripts/New Folder/EnemySpawner.cs
+++ EDIProjectTemplate/Assets/_Scripts/New Folder/EnemySpawner.cs
@@ -1,27 +1,15 @@
 using System;
 using System.Collections.Generic;
 using _Scripts.Units.Enemy;
+using _Scripts.Units.Player.Core;
 using UnityEngine;
 
-public class EnemySpawner : MonoBehaviour
+public class EnemySpawner : MonoBehaviour, IInteractable
 {
     [SerializeField] private AiAgent AiAgent;
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public void Interact()
     {
-        
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if(other.CompareTag("Player"))
-             Instantiate(AiAgent, transform.position, transform.rotation);
+        Instantiate(AiAgent, transform.position, transform.rotation);
     }
 }
