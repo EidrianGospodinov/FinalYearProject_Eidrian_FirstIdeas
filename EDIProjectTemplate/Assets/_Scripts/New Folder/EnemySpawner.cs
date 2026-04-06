7c4853929c49da597c15409fe98c4253baa455cd
Edko03 <121055810+EidrianGospodinov@users.noreply.github.com>
2026-04-06 14:17:13 +0100

    zenject does not inject data int a normal Instantiate function, use DiContainer instead

--- EDIProjectTemplate/Assets/_Scripts/New Folder/EnemySpawner.cs
+++ EDIProjectTemplate/Assets/_Scripts/New Folder/EnemySpawner.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using _Scripts.Units.Enemy;
 using _Scripts.Units.Player.Core;
 using UnityEngine;
+using Zenject;
 
 public class EnemySpawner : MonoBehaviour, IInteractable
 {
     [SerializeField] private AiAgent AiAgent;
-
+    [Inject]
+    private DiContainer _container;
     public void Interact()
     {
-        Instantiate(AiAgent, transform.position, transform.rotation);
+        _container.InstantiatePrefab(AiAgent, transform);
+        //Instantiate(AiAgent, transform.position, transform.rotation);
     }
 }
