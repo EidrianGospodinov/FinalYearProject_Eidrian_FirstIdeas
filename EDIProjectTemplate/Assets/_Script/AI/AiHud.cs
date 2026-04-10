a376a91c9f47fb70e04b53e2245633151c72afc9
Edko03 <121055810+EidrianGospodinov@users.noreply.github.com>
2026-04-10 18:54:20 +0100

    Changing the health bar visuals for all enemies to be in world space and show the status effect over their head

---
+++ EDIProjectTemplate/Assets/_Script/AI/AiHud.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using _Scripts.StateMachine.EnemyStatemMachine;
+using _Scripts.Units.Enemy;
+using _Scripts.Units.Player;
+using TMPro;
+using UnityEngine;
+
+namespace _Script.AI
+{
+    public class AiHud : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI statusText;
+
+        private Health agentHealth;
+        private AiAgent agent;
+        
+        private string agentID;
+        private Transform mainCamTransform;
+        private bool useAStar = false;
+
+        private void Start()
+        {
+            mainCamTransform = Camera.main.transform;
+            agent = GetComponentInParent<AiAgent>();
+            agentHealth = GetComponentInParent<Health>();
+            if (agentHealth == null || agent == null)
+            {
+                Debug.LogError("AiHUD does not have health or agent components.");
+                return;
+            }
+
+            agentID = agent.agentConfig.name;
+
+
+            UpdateDisplay();
+        }
+        void LateUpdate()
+        {
+           
+            //keep the text facing the camera
+            transform.LookAt(transform.position + mainCamTransform.rotation * Vector3.forward,
+                mainCamTransform.rotation * Vector3.up);
+            
+            UpdateDisplay();
+        }
+
+        private void UpdateDisplay()
+        {
+            if (agent.stateMachine == null)
+            {
+                return;
+            }
+
+            var stateId = agent.stateMachine.CurrentStateId;
+            statusText.text =
+                $"ID: {agentID + agent.GetUniqueID()}\n" +
+                $"HP: {Mathf.CeilToInt(agentHealth.currentHealth)}\n" +
+                $"STATE: {stateId}";
+
+
+
+
+            //Change color based on HP percentage
+            if (agentHealth.currentHealth <= agentHealth.maxHealth * 0.25f)
+            {
+                statusText.color = Color.red;
+            }
+            else if (stateId == EnemyStateId.AttackGeneric || stateId == EnemyStateId.Charge || stateId == EnemyStateId.ReadyToAttack) 
+            {
+                statusText.color = Color.yellow;
+            }
+            else
+            {
+                statusText.color = Color.white;
+            }
+        }
+    }
+}
