8655f1b9f9aa615d6eed14f521eaf82f15637804
Edko03 <121055810+EidrianGospodinov@users.noreply.github.com>
2026-04-25 14:46:47 +0100

    fixing the cyclops having to big of normals when highlighted

--- EDIProjectTemplate/Assets/_Scripts/Units/Enemy/View/ChangeLayerChildren.cs
+++ EDIProjectTemplate/Assets/_Scripts/Units/Enemy/View/ChangeLayerChildren.cs
@@ -1,10 +1,14 @@
 using UnityEngine;
-
+using UnityEngine.Serialization;
 
 
 public class ChangeLayerChildren : MonoBehaviour
 {
+    [SerializeField] private float normalOutlineSize = 0.02f;
+    [SerializeField] private float cyclopsOutlineSize = 0.0001f;
+    [SerializeField] private bool useCyclopsSize = false;
     
+    private static readonly int SizeID = Shader.PropertyToID("_Size");
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -20,9 +24,21 @@ public class ChangeLayerChildren : MonoBehaviour
 
     public void ChangeLayerOfChildren(LayerMask layerMask)
     {
+        int layer = (int)Mathf.Log(layerMask.value, 2);
+
+        float outlineSize = useCyclopsSize ? cyclopsOutlineSize : normalOutlineSize;
         foreach (Transform child in transform)
         {
-            child.gameObject.layer = (int)Mathf.Log(layerMask.value, 2);
+            child.gameObject.layer = layer;
+            
+            Renderer renderer = child.GetComponent<Renderer>();
+            if (renderer != null)
+            {
+                MaterialPropertyBlock block = new MaterialPropertyBlock();
+                renderer.GetPropertyBlock(block);
+                block.SetFloat(SizeID, outlineSize);
+                renderer.SetPropertyBlock(block);
+            }
         }
     }
 }
